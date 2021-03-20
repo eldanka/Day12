75f52642deb0d84a010e8be6c4541b9e27695289
eldanka <eldankahrovic2208@gmail.com>
2021-03-20 20:43:22 +0100

    Adding ex07

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -27,27 +27,24 @@ var collection = {
 var oldCollection = JSON.parse(JSON.stringify(collection));
 
 function updateRecords(object, id, prop, value) {
-    if(prop != "tracks" && value != "") {
-        collection[id][prop](value);
-        return object;
-    }
-    if(prop == "tracks" && collection[id].hasOwnProperty(prop) == false) {
-        collection[id][prop] = [];
-        collection[id][prop].push(value);
-        return object;
-    }
-    if(prop == "tracks" && value != "") {
-        collection[id][prop].push(value);
-        return object;
-    }
-    if(value == "")  {
-        delete collection[id][prop];
-        return object;
-    }
+    if(prop === "tracks" && !collection[id].hasOwnProperty([prop])){
+        object[id][prop] = [];
+        object[id][prop].push(value);
+      }else if(prop === "tracks" && collection[id].hasOwnProperty([prop])){
+        object[id][prop].push(value);
+      }else{
+        object[id][prop] = value;
+      }
+      
+      if(!value){
+        delete object[id][prop];
+      }
+      
+      return object;
 }
 
 console.log(updateRecords(collection, 5439, "artist", "ABBA"));
 console.log(updateRecords(collection, 5439, "tracks", "Take a Chance on Me"));
 console.log(updateRecords(collection, 2548, "artist", ""));
-console.log(updateRecords(collection, 1245, "tracks", "ABBA"));
+console.log(updateRecords(collection, 1245, "tracks", "Addicted to Love"));
 module.exports = updateRecords;
